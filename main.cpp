26079aa252b01efc858b4194f3a1447eb6838df9
Zachary Winter <winterzachary19@gmail.com>
2022-09-03 02:10:21 -0500

    2nd push to make sure everything updates!

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+//test 2nd push.
+/*  if you're reading this you're prob cool or something idk
+    and for going through my old work you must be really bored or really curious
+    whatever the reason is I appreciate it, never hesitate to reach out.
+    -Zach W.  
+*/
+int main() {
+
+
+    std::cout << "Hello, Github" << std::endl;
+
+    
+    return 0;
+}
